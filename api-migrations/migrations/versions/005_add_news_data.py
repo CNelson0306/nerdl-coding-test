6c9d4af185f4bd5d9c15e34514d2c3d4fe9ff31d
Carl Nelson <99733202+CNelson0306@users.noreply.github.com>
2025-06-01 21:15:33 +0100

    First Commit

--- api-migrations/migrations/versions/005_add_news_data.py
+++ api-migrations/migrations/versions/005_add_news_data.py
@@ -13,55 +13,51 @@ depends_on = None
 def upgrade():
     connection = op.get_bind()
 
-    connection.execute(
-        sa.text("""
-                INSERT INTO news_articles (title, excerpt, author, category, image_base64, published_date)
-                VALUES
-                (
-                    :title1, :excerpt1, :author1, :category1, :image1, :published1
-                ),
-                (
-                    :title2, :excerpt2, :author2, :category2, :image2, :published2
-                ),
-                (
-                    :title3, :excerpt3, :author3, :category3, :image3, :published3
-                ),
-                (
-                    :title4, :excerpt4, :author4, :category4, :image4, :published4
-                ),
-            """),
-           {
+    articles = [
+            {
                 "title1": "Breaking News: Falcon Takes Off",
                 "excerpt1": "Falcon framework simplifies web APIs.",
                 "author1": "Jane Doe",
                 "category1": "Tech",
                 "image1": None,
                 "published1": 2025,
-
+            },
+            {
                 "title2": "React Leads The Way",
                 "excerpt2": "Why React is being used by so many employers.",
                 "author2": "Bob",
                 "category2": "Development",
                 "image2": None,
                 "published2": 2025,
-
+            },
+            {
                 "title3": "Engineering Solutions Driving Results",
                 "excerpt3": "Tech company specialising in building scalable software solutions.",
                 "author3": "John Doe",
                 "category3": "Business",
                 "image3": None,
                 "published3": 2025,
-
+            },
+            {
                 "title4": "Blockchain Solutions Driving Real-World Impact",
                 "excerpt4": "Cryto giants building secure, scalable blockchain infrastructure",
                 "author4": "Sue Doe",
                 "image4": None,
                 "published4": 2025
             }
+        ]
+
+    connection.execute(
+        sa.text("""
+            INSERT INTO news_articles (title, excerpt, author, category, image_base64, published_date)
+            VALUES (:title, :excerpt, :author, :category, :image, :published)
+        """),
+        articles
     )
 
 def downgrade():
-    op.drop_table('articles') 
+    connection = op.get_bind()
+    connection.execute(sa.text("DELETE FROM news_articles WHERE author IN ('Jane Doe', 'Bob', 'John Doe', 'Sue Doe')"))
 
     
 
