83047002bed571bae0403ce191d94bdbeded8e50
Carl Nelson <99733202+CNelson0306@users.noreply.github.com>
2025-06-02 13:35:08 +0100

    First Commit

--- web/app/src/components/News.js
+++ web/app/src/components/News.js
@@ -8,68 +8,63 @@ import img3 from '../assets/img3.png';
 
 function News() {
 
-  return (
-    <>
-    <h2 className='news-heading'>News</h2>
-        <section className="news-section">
-          <div className="news-articles">
-            {/* News articles will go here */}          
-          
-          <div className='latest-news'>
-              <h3>Latest News</h3>
-            <div className='latest-news-container'>
-              <article className="latest-news-article">
-                <img src={img1} alt='Bitcoin image' />
-            <div className='news-content'>
+    return (
+        <>
+        <h2 className='news-heading'>News</h2>
+            <section className="news-section">
+            <div className="news-articles">
+                {/* News articles will go here */}          
+            
+            <div className='latest-news'>
+                <h3>Latest News</h3>
+                <div className='latest-news-container'>
+                <article className="latest-news-article">
+                    <img src={img1} alt='Bitcoin image' />
+                <div className='news-content'>
+                    <h3>Bitcoin</h3>
+                        <p><span>&#127919;</span> Feeling Lucky: James Wynn's $1bn Bitcoin Bet Ends in a $60m Flameout</p>
+                <div className='read-more'>
+                    <h3>Just Now</h3>
+                    <button className='read-more-btn'>read more</button>
+                </div>
+                <h4>G.Lomas</h4>
+                    </div>
+                </article>
+                </div>
+            </div>
+            </div>
+
+            <div className='trending-news'>
+            <h3>Trending</h3>
+            <article className='trending-article'>
+                <div className='trending-content'>
                 <h3>Bitcoin</h3>
                     <p><span>&#127919;</span> Feeling Lucky: James Wynn's $1bn Bitcoin Bet Ends in a $60m Flameout</p>
-              <div className='read-more'>
-                <h3>Just Now</h3>
-                <button className='read-more-btn'>read more</button>
-              </div>
-              <h4>G.Lomas</h4>
+                <h4>G.Lomas</h4>
                 </div>
-              </article>
-              </div>
-          </div>
-          </div>
+                <img src={img1} alt='Bitcoin Image' />
+            </article>
 
-          
+            <article className='trending-article'>
+                <div className='trending-content'>
+                <h3>Finance</h3>
+                    <p><span>&#129534;</span> Hyperliquid Makes Its Case for 24/7 Cryto Derivatives Trading to US</p>
+                <h4>G.Lomas</h4>
+                </div>
+                <img src={img2} alt='Crypto Image' />
+            </article>
 
-          <div className='trending-news'>
-           <h3>Trending</h3>
-
-          <article className='trending-article'>
-            <div className='trending-content'>
-              <h3>Bitcoin</h3>
-                <p><span>&#127919;</span> Feeling Lucky: James Wynn's $1bn Bitcoin Bet Ends in a $60m Flameout</p>
-              <h4>G.Lomas</h4>
+            <article className='trending-article'>
+                <div className='trending-content'>
+                <h3>Cryptocurrency</h3>
+                    <p><span>&#128747;</span> Crypto Travellers Spend Big - And Often</p>
+                <h4>L.Abate</h4>
+                </div>
+                <img src={img3} alt='Crypto Travellers Image' />
+            </article> 
             </div>
-            <img src={img1} alt='Bitcoin Image' />
-          </article>
-
-          <article className='trending-article'>
-            <div className='trending-content'>
-              <h3>Finance</h3>
-                <p><span>&#129534;</span> Hyperliquid Makes Its Case for 24/7 Cryto Derivatives Trading to US</p>
-              <h4>G.Lomas</h4>
-            </div>
-            <img src={img2} alt='Crypto Image' />
-          </article>
-
-          <article className='trending-article'>
-            <div className='trending-content'>
-              <h3>Cryptocurrency</h3>
-                <p><span>&#128747;</span> Crypto Travellers Spend Big - And Often</p>
-              <h4>L.Abate</h4>
-            </div>
-            <img src={img3} alt='Crypto Travellers Image' />
-          </article>
-          
-            
-        </div>
-        </section>
-    </>
+            </section>
+        </>
   )
 }
 
